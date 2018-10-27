75838e789443dac660f8fcfe5729a0247b221279
= <=>
2018-10-27 16:50:44 -0400

    Add voice recog profile creation and enrollment

--- app/main/service/register_service.py
+++ app/main/service/register_service.py
@@ -10,6 +10,7 @@ import os
 import json
 from app.main import db
 from app.main.model.users import Users
+import http.client, urllib.request, urllib.parse, urllib.error, base64
 
 
 def register_biometrics(settings,data):
@@ -17,17 +18,18 @@ def register_biometrics(settings,data):
     # for setting in settings:
     switcher={
                 "QR_CODE":verify_qr_code(data=data),
-                "FINGERPRINT":verify_fingerprint(data=data)
+                "FINGERPRINT":verify_fingerprint(data=data),
+                "FACERECOG":'hello',
+                "VOICERECOG": enroll_voice(data=data)
             }
-    return switcher.get("QR_CODE","Invalid biometric setting")
+    return switcher.get("VOICERECOG","Invalid biometric setting")
     # user = Users.query.filter_by(email=data['email']).first()
     
 def verify_qr_code(data):
 
+    return "hello"
+
     randomId = str(uuid.uuid4())
-
-    #we need to save the qr code by updating a recently created biometrics instance via its id. We are going to do this for every biometric registration.
-
 
     #Now we create the qr_code image
     qr = pyqrcode.create(randomId, error='L', version=5, mode='binary')
@@ -71,6 +73,71 @@ def verify_face(data):
     db.session.add(data)
     db.session.commit()
 
-def verify_voice(data):
-    db.session.add(data)
-    db.session.commit()
+
+# Enrolls a newly created profile.
+def enroll_voice(data):
+     
+     #Create first an enrollment profile
+     profile_string = json.loads(create_voice_profile())
+
+     if profile_string:
+
+         profile_id = profile_string['verificationProfileId']
+         #Now we save the profile string in our database. We may update the row, not create a new one, so we need to find by person's id
+
+         
+         
+         #-------------------TODO-----------------------------
+         #Create an enrollment. To enroll a person we need to enroll the voice 3 times.
+         headers = {
+            # Request headers
+            'Content-Type': 'multipart/form-data',
+            'Ocp-Apim-Subscription-Key': '9cad2c86ad8e4220ae02edc989424cac',
+         }
+
+         params = urllib.urlencode({
+         })
+
+         #Lets enroll the person 3 times. We could use 3 diferent voice clips or just one.
+
+
+         # Here we open a 16k rate, 16 bit, mono WAV audio file. We need to check how files are received in flask.
+         body = open('test/valentina/Wavs/valentina_a_1.wav', 'rb')
+
+         conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+         conn.request("POST", "/spid/v1.0/verificationProfiles/{profile_id}/enroll?%s" %
+                 params, body, headers)
+         response = conn.getresponse()
+         data = response.read()
+         return data
+         conn.close()
+
+
+     else:
+         return "New profile could not be created"  
+
+
+
+#Creates a new enrollment profile
+def create_voice_profile():
+    headers = {
+        # Request headers
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': '9cad2c86ad8e4220ae02edc989424cac',
+    }
+
+    params = urllib.parse.urlencode({
+    })
+
+    body = {
+        "locale": "en-us",
+    }
+
+    conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
+    conn.request("POST", "/spid/v1.0/verificationProfiles?%s" %
+                 params, str(body), headers)
+    response = conn.getresponse()
+    voice_profile = response.read()
+    return voice_profile.decode('utf-8')
+    conn.close()
+
